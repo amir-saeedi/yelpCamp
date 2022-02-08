42178c8d232d6d82d9bb2f4a2bbd2befd85dc3b8
amir-saeedi <saeediamir1381@gmail.com>
2022-02-09 01:09:52 +0330

    redy for deploying

---
+++ routes/reviews.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+const Campground = require("../models/campground");
+const Review = require("../models/review");
+
+const reviews = require("../controllers/reviews");
+
+const { reviewSchema } = require("../schemas.js");
+
+const catchAsync = require("../utils/catchAsync");
+
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.creatReview));
+
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  catchAsync(reviews.deleteReview)
+);
+
+module.exports = router;
